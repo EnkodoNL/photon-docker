264a7bcd6f4dac83a0578b2015e40f8ac12cb3b2
Sebastiaan Pasma <sebastiaan@enkodo.nl>
2025-09-26 16:20:19 +0200

    save to data disk

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,13 @@
 
 # Download elasticsearch index
 if [ ! -d "/photon/photon_data/elasticsearch" ]; then
-    echo "Downloading search index"
+    echo "Downloading search index to /photon/photon_data"
+    
+    # Create data directory if it doesn't exist
+    mkdir -p /photon/photon_data
+    
+    # Change to data directory before extracting
+    cd /photon/photon_data
 
     # Let graphhopper know where the traffic is coming from
     USER_AGENT="docker: photon-geocoder"
@@ -11,6 +17,9 @@ if [ ! -d "/photon/photon_data/elasticsearch" ]; then
     # Take a look at http://download1.graphhopper.com/public/extracts/by-country-code for your country
     # wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/extracts/by-country-code/nl/photon-db-nl-latest.tar.bz2 | bzip2 -cd | tar x
     wget --user-agent="$USER_AGENT" -O - http://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | bzip2 -cd | tar x
+    
+    # Return to photon directory
+    cd /photon
 fi
 
 # Start photon if elastic index exists
